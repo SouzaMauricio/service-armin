636ad8c92718a558350a3f2d797da5b01dafbc1e
Mauricio <mauricio.pereira@mobly.com.br>
2022-05-25 00:43:46 -0300

    feat: add cor

--- server.ts
+++ server.ts
@@ -1,16 +1,10 @@
 import express, { Express } from 'express'
-import dotenv from 'dotenv'
 import { routes } from './src/router/routes'
-// import cors from 'cors'
-
-dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
 
 app.use(routes)
-
-// app.use(cors())
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${String(port)}`)
