19027b9b765ff8b72d62204e915c19511bb3dacf
Mauricio <mauricio.pereira@mobly.com.br>
2022-06-04 02:16:02 -0300

    feat: add form data and aws sdk

--- server.ts
+++ server.ts
@@ -1,9 +1,18 @@
 import express, { Express } from 'express'
 import { routes } from './src/router/routes'
+import formData from 'express-form-data'
+import os from 'os'
 
 const app: Express = express()
 const port = process.env.PORT
-
+const options = {
+  uploadDir: os.tmpdir(),
+  autoClean: true
+}
+app.use(formData.parse(options))
+app.use(formData.format())
+app.use(formData.stream())
+app.use(formData.union())
 app.use(routes)
 
 app.listen(port, () => {
