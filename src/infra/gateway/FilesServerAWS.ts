47fb107c4ac169b7ecdd12ed586eaf7d85c2f9ad
Mauricio <mauricio.pereira@mobly.com.br>
2022-06-09 01:07:49 -0300

    refact: temp adjustments

--- src/infra/gateway/FilesServerAWS.ts
+++ src/infra/gateway/FilesServerAWS.ts
@@ -1,4 +1,5 @@
 import AWS from 'aws-sdk'
+import S3 from 'aws-sdk/clients/s3'
 import { IFilesServer } from '../../domain/infra/gateway/IFilesServer'
 import fs from 'fs'
 
@@ -11,7 +12,7 @@ export class FilesServerFilesServerAWSGateway implements IFilesServer {
 
   async uploadFiles (body: any, fileName: string, serverPath: string): Promise<any> {
     const fileContent = fs.readFileSync(body.image.path)
-    const s3 = new AWS.S3()
+    const s3 = new S3()
     const key = `${serverPath}${String(fileName)}`
     const s3Params = {
       Bucket: String(process.env.AWS_BUCKET_PICTURES),
@@ -28,7 +29,7 @@ export class FilesServerFilesServerAWSGateway implements IFilesServer {
   }
 
   async deleteFile (key: string): Promise<void> {
-    const s3 = new AWS.S3()
+    const s3 = new S3()
     const s3Params = {
       Bucket: String(process.env.AWS_BUCKET_PICTURES),
       Key: key
