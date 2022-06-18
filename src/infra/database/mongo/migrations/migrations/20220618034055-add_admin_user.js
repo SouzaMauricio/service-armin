2c0011f75c0d65fdcd7617aa507f81318b4f18e7
Mauricio <mauricio.pereira@mobly.com.br>
2022-06-18 01:47:49 -0300

    refact: small adjustment

--- src/infra/database/mongo/migrations/migrations/20220618034055-add_admin_user.js
+++ src/infra/database/mongo/migrations/migrations/20220618034055-add_admin_user.js
@@ -6,7 +6,7 @@ module.exports = {
       email: 'admin@email.com',
       password: '$2a$10$368YeIo9RI5G29BPegLXi.JiI23lKrfGl.pu3r6O0QpDuTE/r7bq6', // administrador
       image: {
-        path: 'https://picsum.photos/200'
+        fullPath: 'https://picsum.photos/300'
       }
     })
   },
